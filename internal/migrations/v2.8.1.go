4de4acee4f3bb106638bd4af99ed67c9f6261576
pat-s <patrick.schratz@gmail.com>
2026-08-14 16:07:41 +0000

    fix(authz): restore custom attributes for admins

---
+++ internal/migrations/v2.8.1.go
@@ -0,0 +1,18 @@
+package migrations
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/knadh/koanf/v2"
+	"github.com/knadh/stuffbin"
+)
+
+// V2_8_1 restores custom-attribute management on legacy Admin roles.
+func V2_8_1(db *sqlx.DB, _ stuffbin.FileSystem, _ *koanf.Koanf) error {
+	_, err := db.Exec(`
+		UPDATE roles
+		SET permissions = array_append(permissions, 'custom_attributes:manage')
+		WHERE name = 'Admin'
+		  AND NOT ('custom_attributes:manage' = ANY(permissions));
+	`)
+	return err
+}
