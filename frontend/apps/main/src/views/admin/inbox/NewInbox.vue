bb5c98906659e163417cde50e863bdcab3ae0409
Abhinav Raut <abhinavrautcs@gmail.com>
2026-09-17 01:53:22 +0530

    remove live chat beta badge

--- frontend/apps/main/src/views/admin/inbox/NewInbox.vue
+++ frontend/apps/main/src/views/admin/inbox/NewInbox.vue
@@ -108,8 +108,7 @@ const channels = [
     title: t('globals.terms.liveChat'),
     subTitle: t('admin.inbox.createLiveChatInbox'),
     onClick: selectLiveChatChannel,
-    icon: MessageCircle,
-    badge: t('globals.terms.beta')
+    icon: MessageCircle
   },
   {
     title: t('globals.terms.whatsapp'),
